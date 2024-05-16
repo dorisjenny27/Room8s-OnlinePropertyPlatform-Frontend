f63926feacb9a5ef56ec28baa7687acfc6a552ee
Oluwabanke Eboda <abeegarl@gmail.com>
2024-05-16 18:49:35 +0100

    Move ConfirmPassword Page to Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar"
+import ConfirmPassword from "./pages/ConfirmPassword"
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <ConfirmPassword/>
     </>
   )
 }
