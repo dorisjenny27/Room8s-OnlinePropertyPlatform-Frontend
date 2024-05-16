d7b36d698bd269f713854773c2a2083a18e2474a
Oluwabanke Eboda <abeegarl@gmail.com>
2024-05-16 18:52:22 +0100

    Initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import Navbar from "./components/Navbar"
-import ConfirmPassword from "./pages/ConfirmPassword"
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <ConfirmPassword/>
+     
     </>
   )
 }
