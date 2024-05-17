efdb28c86afa4041c00645be5633da1d0f940a38
Oluwabanke Eboda <abeegarl@gmail.com>
2024-05-17 20:36:48 +0100

    Add Confirm Password page route

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import {
 
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layout/MainLayout";
+import ConfirmPassword from "./pages/ConfirmPassword";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />} >
-      <Route index element={<HomePage />} />
+    <Route index element={<HomePage />} />
+    <Route path="/ConfirmPassword" element={<ConfirmPassword/>}/> 
     </Route>
   )
 );
